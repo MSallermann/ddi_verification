cc2bee809601dda60832a97d97c1b03c06d256d1
M. Sallermann <M.Sallermann@fz-juelich.de>
2018-09-12 15:09:32 +0200

    Added comparison to brute force energy

--- test_all.py
+++ test_all.py
@@ -1,10 +1,34 @@
 # -->> Set path to spirit python pkg here <<--
-path_to_spirit_pkg = "/home/moritz/Coding/spirit/core/python"
-
+path_to_spirit_pkg = "/path/to/spirit/python"
 import sys
 sys.path.append(path_to_spirit_pkg)
 
-from test_cases import *
+from test_cases import test_brute_force
+
+number_of_tests = 0
+number_of_passed = 0
+passed = []
+failed = []
+
+print(">>> TEST: " + test_brute_force.name + "\n")
+
+if test_brute_force.run():
+    passed.append(test_brute_force.name)
+else:
+    failed.append(test_brute_force.name)
+
+print("\n>--------------------------------<")
+print(  "              SUMMARY             ")
+print(  ">--------------------------------<")
+print(">>> Passed {0} out of {1} tests".format(len(passed), len(passed) + len(failed)))
+print("\n>>> Failed tests:")
+for name in failed:
+    print(" -- " + name)
+print("\n>>> Passed tests:")
+for name in passed:
+    print(" -- " + name)
+print(  ">--------------------------------<")
+
 
 
 
--- test_cases/test_brute_force.py
+++ test_cases/test_brute_force.py
@@ -1,11 +1,77 @@
-from spirit import state, simulation, constants, geometry
+from spirit import state, simulation, constants, geometry, system, hamiltonian, configuration
+import numpy as np
 
-print("\n>>> TEST 1: Comparison to brute force calculation <<<\n")
+mu_0 = 2.01335452495e-28
+mu_B = 0.0578838177025
+name = "Energy comparison to brute force calculation"
 
-inputfile = "test_cases/input/input_brute_force.cfg"
-with state.State(inputfile, quiet = True) as p_state:
-    simulation.start(p_state, simulation.METHOD_LLG, simulation.SOLVER_SIB, n_iterations=10, single_shot = True)
-    simulation.single_shot(p_state)
-    simulation.stop_all(p_state)
+def E_DDI_BF(pos, spins, mu_s = 1):
+    E = 0
+    mult = - mu_0 * mu_B**2 / (4 * np.pi) * 10e29
+    for i in range(len(pos)):
+        for j in range(i+1, len(pos)):
+            r = pos[i] - pos[j]
+            d = np.linalg.norm(r)
+            r /= d
+            # E += mult/d**3 * mu[i] * mu[j] * ( 3 * np.dot(spins[i], r) * np.dot(spins[j], r) - np.dot(spins[i], spins[j]) )
+            E += mult/d**3 * mu_s * ( 3 * np.dot(spins[i], r) * np.dot(spins[j], r) - np.dot(spins[i], spins[j]) )
+    return E
+
+def test_energy(p_state):
+    nos = system.get_nos(p_state)
+    pos = np.array(geometry.get_positions(p_state)).reshape(nos, 3)
+    spins = np.array(system.get_spin_directions(p_state)).reshape(nos, 3)
+
+    E_BF = E_DDI_BF(pos, spins)
+    E_Spirit = system.get_energy(p_state)
+
+    return E_BF, E_Spirit
+
+def run():
+    inputfile = "test_cases/input/input_brute_force.cfg"
+    passed = True
+
+    with state.State(inputfile, quiet = True) as p_state:
+        hamiltonian.set_boundary_conditions(p_state, [0, 0, 0], idx_image=-1, idx_chain=-1)
+
+        hamiltonian.set_field(p_state, 0.0, [0, 0 , 1])
+        hamiltonian.set_anisotropy(p_state, 0.0, [0, 0 , 1])
+        hamiltonian.set_anisotropy(p_state, 0.0, [0, 0 , 1])
+
+        # simulation.start(p_state, simulation.METHOD_LLG, simulation.SOLVER_SIB, n_iterations=10, single_shot = True)
+
+        configuration.plus_z(p_state)
+        system.update_data(p_state)
+        E_Bf, E_Spirit = test_energy(p_state)
+        passed = True
+        print('\n>>> Result 1: plus_z')
+        print('Brute Force = ', E_Bf)
+        print('Spirit      = ', E_Spirit)
+        if np.abs(E_Bf - E_Spirit) > 1e-6:
+            passed = False
+
+        configuration.random(p_state)
+        system.update_data(p_state)
+        E_Bf, E_Spirit = test_energy(p_state)
+        passed = True
+        print('\n>>> Result 2: random')
+        print('Brute Force = ', E_Bf)
+        print('Spirit      = ', E_Spirit)
+        if np.abs(E_Bf - E_Spirit) > 1e-6:
+            passed = False
+
+        configuration.hopfion(p_state, 4)
+        system.update_data(p_state)
+        E_Bf, E_Spirit = test_energy(p_state)
+        passed = True
+        print('\n>>> Result 3: hopfion')
+        print('Brute Force = ', E_Bf)
+        print('Spirit      = ', E_Spirit)
+        if np.abs(E_Bf - E_Spirit) > 1e-6:
+            passed = False
+        
+        return passed
+
+
 
 
